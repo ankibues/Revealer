9680a68b24bc6a6e9c704cef9964da82766d6e01
Ankit <ankitbhandariiitr@gmail.com>
2024-01-25 18:30:51 -0500

    cosmetic changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import '../styles/Modal.css'; 
 
-const Modal = ({ show, score, onRestart }) => {
+const Modal = ({ show, score, answer, message}) => {
   if (!show) return null;
 
   return (
     <div className="modal">
       <div className="modalContent">
-        <h2>Congratulations!</h2>
-        <p>Your final score is: {score}</p>
-        <button>Come back tomorrow for another reveal!</button>
+        <p>{message}</p>
+        <p>  Correct answer is '{answer}' </p>
+        {score !== null ? <p>Your final score is: {score}</p> : null}
+        <p>See you tomorrow !</p>
       </div>
     </div>
   );
