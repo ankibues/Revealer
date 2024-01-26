5cebc11e5e1013ac942e30eb8e0058a452e908fb
Ankit <ankitbhandariiitr@gmail.com>
2024-01-26 14:57:40 -0500

    modal_updates

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import '../styles/Modal.css'; 
 
-const Modal = ({ show, score, answer, message}) => {
+const Modal = ({ onClose, show, score, answer, message}) => {
   if (!show) return null;
 
   return (
-    <div className="modal">
+    <div className="modal-backdrop">
       <div className="modalContent">
+      <button className="modal-close-button" onClick={onClose} onKeyDown={(e) => {
+    // Check if the key pressed is 'Enter' or 'Escape' and close the modal
+    if (e.key === 'Enter' || e.key === 'Escape') onClose();
+  }}
+  tabIndex="0"
+  aria-label="Close"
+> &times;</button>
         <p>{message}</p>
         <p>  Correct answer is '{answer}' </p>
         {score !== null ? <p>Your final score is: {score}</p> : null}
